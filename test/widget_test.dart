9bd05ec0b556c8b5d7ef6f996f4b17b96099b624
Yusuf Yılıkoğlu <yusuf.y0079@icloud.com>
2026-03-28 12:23:27 +0300

    ana sayfa ve rapor seçim  ekranlari tasarlandı

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:medikal_rapor_tercuman/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MedikalTercumanApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
