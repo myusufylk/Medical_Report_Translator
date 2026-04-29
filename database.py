5148246ec095204c2cb2688eaca1b911e68fda1b
Cemre Yurtsever <90215801+cemreeyrtsvr@users.noreply.github.com>
2026-04-30 00:49:42 +0300

    Use environment variable for DATABASE_URL
    Updated DATABASE_URL to use environment variable.

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/medical"
-
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@localhost:5432/medical")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
